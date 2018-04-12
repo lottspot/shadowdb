2abb32599e6359172f28842f3659247da678d9ec
James Lott <james@lottspot.com>
2018-04-12 16:44:07 -0400

    Accept interfaces in shadowDB function arguments

--- shadowdb.go
+++ shadowdb.go
@@ -4,29 +4,48 @@ import (
   "io"
 )
 
+type ShadowUser interface {
+  AsRecord() string
+  Uname() string
+  Pwhash() string
+  LastChange() int
+  MinAge() int
+  MaxAge() int
+  WarnDays() int
+  GraceDays() int
+  Expires() int
+  SetUname(uname string)
+  SetPwhash(pwhash string)
+  SetMinAge(minAge int)
+  SetMaxAge(maxAge int)
+  SetWarnDays(warnDays int)
+  SetGraceDays(graceDays int)
+  SetExpires(expires int)
+}
+
 type shadowDB struct {
-  records []shadowUser
+  records []ShadowUser
 }
 
 func NewDB() *shadowDB {
   db := new(shadowDB)
-  db.records = make([]shadowUser, 0)
+  db.records = make([]ShadowUser, 0)
   return db
 }
 
-func (db *shadowDB) Load(src io.Reader) error {
+func (db *shadowDB) Load(r io.Reader) error {
   return nil
 }
 
-func (db *shadowDB) Dump(dst io.Writer) error {
+func (db *shadowDB) Dump(w io.Writer) error {
   return nil
 }
 
-func (db *shadowDB) User(uname string) shadowUser {
-  return shadowUser{}
+func (db *shadowDB) User(uname string) *shadowUser {
+  return new(shadowUser)
 }
 
-func (db *shadowDB) ApplyUser(u *shadowUser) {
+func (db *shadowDB) ApplyUser(u ShadowUser) {
 }
 
 func (db *shadowDB) PurgeUser(uname string) {
@@ -39,5 +58,5 @@ func (db *shadowDB) findRecord(uname string) uint {
 func (db *shadowDB) purgeRecordAt(i uint) {
 }
 
-func (db *shadowDB) insertRecordAt(u *shadowUser, i uint) {
+func (db *shadowDB) insertRecordAt(u ShadowUser, i uint) {
 }
