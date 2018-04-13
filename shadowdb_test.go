c52dafb5345575c92f293adb72ef90913e01c149
James Lott <james@lottspot.com>
2018-04-13 11:27:02 -0400

    Add test for insertRecordAt

--- shadowdb_test.go
+++ shadowdb_test.go
@@ -11,4 +11,20 @@ type stubDBRecord struct {
 func (s stubDBRecord) Record() string {if s.record == nil {return ""} else {return s.record()}}
 func (s stubDBRecord) Uname() string {if s.uname == nil {return ""} else {return s.uname()}}
 
-func TestInsertRecordAt(t *testing.T) {}
+func TestInsertRecordAt(t *testing.T) {
+  records := []DBRecord{
+    stubDBRecord{},
+    stubDBRecord{},
+    stubDBRecord{},
+  }
+  db := shadowDB{
+    records: records,
+  }
+  newRecord := stubDBRecord{
+    uname: func() (string) {return "user"},
+  }
+  db.insertRecordAt(newRecord, 1)
+  if db.records[1].Uname() != "user" {
+    t.Error("user did not appear at index 1")
+  }
+}
